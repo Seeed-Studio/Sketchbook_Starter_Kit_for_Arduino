c84344c10e0ebf8723cf1500031a33ebdd6f6b91
hacksaurav <32787860+hacksaurav@users.noreply.github.com>
2018-10-09 21:53:35 +0530

    Update Grove_LED.ino

--- Grove_LED/Grove_LED.ino
+++ Grove_LED/Grove_LED.ino
@@ -1,8 +1,8 @@
 // demo of Grove - Starter V2.0
 // Loovee  2013-3-10
 // as the topic, we will use Grove - Led to make a breath light
-// Grove - LED connect to D3
-// the following pin which support PWM can be used:
+// Grove - LED connect to D3 digital pin 3 of the arduino board
+// the following pin which support PWM(pulse width modulation) can be used:
 // 3, 5, 6, 9, 10, 11
 
 const int pinLed    = 3;                        // pin of led define here
@@ -15,7 +15,7 @@ void setup()
 void loop()
 {
     
-    for(int i=0; i<256; i++)
+    for(int i=0; i<=255; i++)
     {
         analogWrite(pinLed, i);
         delay(5);                               // change delay time can breath faster or slower
